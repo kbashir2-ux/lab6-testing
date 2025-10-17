0268d9910867dc69c0a907f5084c2f959449da99
Khadijah Bashir <232601918+kbashir2-ux@users.noreply.github.com>
2025-10-17 19:22:02 +0000

    Tried different asserts

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -6,31 +6,14 @@ def test_sample_run_anonymizer():
     start = 11
     end = 15
 
-    #result = sample_run_anonymizer("My name is Bond.", 11, 15)
-    result = sample_run_anonymizer(text, start, end)
+    result = sample_run_anonymizer("My name is Bond.", 11, 15)
+    #result = sample_run_anonymizer(text, start, end)
     
     assert result.text == "My name is BIP."
     assert len(result.items) == 1
 
-    item = result.items[0]
-    assert item.start == 11
-    assert item.end == 14 
-    assert item.entity_type == "PERSON"
-    assert item.text == "BIP"
-    assert item.operator == "replace"
-
-'''    
-    assert result.items[0].start == start
-    assert result.items[0].end == 14 
+    assert result.items[0].start == 11
+    assert result.items[0].end == 14
     assert result.items[0].entity_type == "PERSON"
     assert result.items[0].text == "BIP"
     assert result.items[0].operator == "replace"
-
-
-    item = result.items[0]
-    assert item.start == 11
-    assert item.end == 14
-    assert item.entity_type == "PERSON"
-    assert item.text == "BIP"
-    assert item.operator == "replace"
-'''
